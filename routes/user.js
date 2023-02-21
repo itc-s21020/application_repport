00c49309b461af72a670ffdff82e34283515767a
s21020 <s21020@std.it-collere.ac.jp>
2023-02-21 16:33:00 +0900

    loginのレスポンスだけできるようにした

--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,9 @@ const router = express.Router()
 const prisma = require('../index')
 
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
     const {student_code, student_name} = req.body
-    prisma.student.create({
+    await prisma.student.create({
         data: {
             code: student_code,
             name: student_name
@@ -15,6 +15,20 @@ router.post('/register', (req, res) => {
     }).catch(() => {
         res.json({status: 1})
     })
+})
+
+router.post('/login', async (req, res) => {
+    const {student_code} = req.body
+    const student = await prisma.student.findFirst({
+        where: {
+            code: student_code
+        }
+    })
+    if (student == null) {
+        res.json({status: 0})
+    }else {
+        res.json({status: 1})
+    }
 })
 
 module.exports = router
