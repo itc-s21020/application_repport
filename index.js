e1f34cfed33c4ee78ff0d546264d5a88274d5d97
s21020 <s21020@std.it-collere.ac.jp>
2023-02-21 16:13:27 +0900

    ユーザー登録

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const app =express()
+const {PrismaClient} = require('@prisma/client')
+const prisma = new PrismaClient()
+
+module.exports = prisma
 const router = express.Router()
+const userRouter = require('./routes/user')
+
+app.use(express.json())
+
 
 router.get('/', (req, res) =>{
     res.send('hello!!')
 })
 
 app.use(router)
+app.use(userRouter)
 
 app.listen(3000, () => console.log('サーバー起動しました'))
---
+++ routes/user.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+const prisma = require('../index')
+
+
+router.post('/register', (req, res) => {
+    const {student_code, student_name} = req.body
+    prisma.student.create({
+        data: {
+            code: student_code,
+            name: student_name
+        }
+    }).then(() => {
+        res.json({status: 0})
+    }).catch(() => {
+        res.json({status: 1})
+    })
+})
+
+module.exports = router
