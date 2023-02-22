b23fb0158344f035cba7e32df1cbaaf290862851
s21020 <s21020@std.it-collere.ac.jp>
2023-02-22 18:49:18 +0900

    証明書発行申請をできるようにした

--- routes/certificate.js
+++ routes/certificate.js
@@ -6,8 +6,51 @@ router.get('/', (req, res) => {
     res.send('getしました')
 })
 
-router.post('/apply', (req, res) => {
-    res.send('postしました')
+router.post('/apply', async (req, res) => {
+    const {
+        certificate_of_enrollment,
+        transcript,
+        attendance_certificate,
+        certificate_of_expected_graduation,
+        graduation_certificate,
+        health_certificate
+    } = req.body
+    const student = req.session.student
+    await prisma.cerfiticateApplication.create({
+        data: {
+            student_id: student.id,
+            certificate_of_enrollment: certificate_of_enrollment,
+            transcript: transcript,
+            attendance_certificate: attendance_certificate,
+            certificate_of_expected_graduation: certificate_of_expected_graduation,
+            graduation_certificate: graduation_certificate,
+            health_certificate: health_certificate
+        }
+    }).then((result) => {
+        const total_copy =
+            result.certificate_of_enrollment+
+            result.transcript+
+            result.attendance_certificate+
+            result.certificate_of_expected_graduation+
+            result.graduation_certificate+
+            result.health_certificate
+        const total_amount =
+            result.certificate_of_enrollment * 200 +
+            result.transcript * 200 +
+            result.attendance_certificate * 200 +
+            result.certificate_of_expected_graduation * 200 +
+            result.graduation_certificate * 200 +
+            result.health_certificate * 100
+        res.json({
+            status: 0,
+            receipt: req.body,
+            scheduled_date: result.scheduled_date,
+            total_copy: total_copy,
+            total_amount: total_amount
+        })
+    }).catch(() => {
+        res.json({status: 1})
+    })
 })
 
 module.exports = router
