5523db8441c447bd934891677ba8c7fcdeada30f
s21020 <s21020@std.it-collere.ac.jp>
2023-02-22 19:01:54 +0900

    履歴取得作成した

--- routes/certificate.js
+++ routes/certificate.js
@@ -2,8 +2,43 @@ const express = require('express')
 const router = express.Router()
 const prisma = require('../index')
 
-router.get('/', (req, res) => {
-    res.send('getしました')
+router.get('/', async (req, res) => {
+    const student = req.session.student
+    const data = await prisma.cerfiticateApplication.findMany({
+        where: {
+            student_id: student.id
+        }
+    })
+    const applications = data.map((apply) => {
+        const total_copy =
+            apply.certificate_of_enrollment+
+            apply.transcript+
+            apply.attendance_certificate+
+            apply.certificate_of_expected_graduation+
+            apply.graduation_certificate+
+            apply.health_certificate
+        const total_amount =
+            apply.certificate_of_enrollment * 200 +
+            apply.transcript * 200 +
+            apply.attendance_certificate * 200 +
+            apply.certificate_of_expected_graduation * 200 +
+            apply.graduation_certificate * 200 +
+            apply.health_certificate * 100
+        return {
+            id: apply.id,
+            certificate_of_enrollment: apply.certificate_of_enrollment,
+            transcript: apply.transcript,
+            attendance_certificate: apply.attendance_certificate,
+            certificate_of_expected_graduation: apply.certificate_of_expected_graduation,
+            graduation_certificate: apply.graduation_certificate,
+            health_certificate: apply.health_certificate,
+            created_at: apply.created_at,
+            scheduled_date: apply.scheduled_date,
+            total_copy: total_copy,
+            total_amount: total_amount
+        }
+    })
+    res.json(applications)
 })
 
 router.post('/apply', async (req, res) => {
